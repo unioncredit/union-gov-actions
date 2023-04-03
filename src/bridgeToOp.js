0f5dad68fc1ae3a2fbae8fda8fa90140f8457d7c
twygod <twygod@gmail.com>
2023-04-03 14:23:55 +0800

    chg: update script.

--- src/bridgeToOp.js
+++ src/bridgeToOp.js
@@ -4,14 +4,9 @@ const {
   DefenderRelayProvider,
 } = require("defender-relay-client/lib/ethers");
 
-const addressMap = {
-  1: {},
-  5: {
-    treasuryAddr: "",
-    connectorAddr: "0xeC2372EA217437585576cCe877d5B345a743700F",
-    unionAddr: "0x23B0483E07196c425d771240E81A9c2f1E113D3A",
-  },
-};
+const TREASURY_ADDR = "0x8Cf2D155D1b789C72403BAdB33E85664cEF84e6B";
+const CONNECTOR_ADDR = "0xeC2372EA217437585576cCe877d5B345a743700F";
+const UNION_ADDRESS = "0x23B0483E07196c425d771240E81A9c2f1E113D3A";
 
 exports.handler = async function (payload) {
   const provider = new DefenderRelayProvider(payload);
@@ -19,42 +14,25 @@ exports.handler = async function (payload) {
     speed: "average",
   });
 
-  const content = payload.request.body;
-  const events = content.events;
-  for (const evt of events) {
-    const sentinel = evt.sentinel;
-    const addresses = addressMap[sentinel.chainId];
-    // Step 1: Drip Union to ArbConnector
-    const treasury = new ethers.Contract(
-      addresses.treasuryAddr,
-      TREASURY_ABI,
-      signer
-    );
-    const dripTx = await treasury.drip(addresses.connectorAddr);
-    await dripTx.wait();
+  // Step 1: Drip Union to ArbConnector
+  const treasury = new ethers.Contract(TREASURY_ADDR, TREASURY_ABI, signer);
+  const dripTx = await treasury.drip(CONNECTOR_ADDR);
+  await dripTx.wait();
 
-    const union = new ethers.Contract(addresses.unionAddr, ERC20_ABI, signer);
-    const balanceOfUnion = await union.balanceOf(addresses.connectorAddr);
-    console.log(
-      `Connector UNION balance: ${ethers.utils.formatEther(balanceOfUnion)}`
-    );
-    // Don't bridge if the balance is too small
-    if (balanceOfUnion.lte(ethers.utils.parseEther("100"))) return;
+  const union = new ethers.Contract(UNION_ADDRESS, ERC20_ABI, signer);
+  const balanceOfUnion = await union.balanceOf(CONNECTOR_ADDR);
+  console.log(
+    `Connector UNION balance: ${ethers.utils.formatEther(balanceOfUnion)}`
+  );
+  // Don't bridge if the balance is too small
+  if (balanceOfUnion.lte(ethers.utils.parseEther("100"))) return;
 
-    // Step 2: Bridge to Optimism
-    const connector = new ethers.Contract(
-      addresses.connectorAddr,
-      CONNECTOR_ABI,
-      signer
-    );
-    const bridgeTx = await connector.bridge();
-    console.log(
-      `Send Union to Op succeeded! 🙌 ${
-        (await bridgeTx.wait()).transactionHash
-      }`
-    );
-  }
-  return { matches };
+  // Step 2: Bridge to Optimism
+  const connector = new ethers.Contract(CONNECTOR_ADDR, CONNECTOR_ABI, signer);
+  const bridgeTx = await connector.bridge();
+  console.log(
+    `Send Union to Op succeeded! 🙌 ${(await bridgeTx.wait()).transactionHash}`
+  );
 };
 
 const TREASURY_ABI = [
