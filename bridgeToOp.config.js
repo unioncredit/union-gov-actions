39d4806a6d7c6eb486fedfb5d068d35e8bc52c7a
twygod <twygod@gmail.com>
2023-04-03 14:23:55 +0800

    chg: update script.

---
+++ bridgeToOp.config.js
@@ -0,0 +1,18 @@
+// rollup.config.js
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/bridgeToOp.js",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+  },
+  plugins: [
+    commonjs(),
+    resolve(),
+    json(),
+    // terser(),
+  ],
+};
