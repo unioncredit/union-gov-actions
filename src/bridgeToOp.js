6af177f1f080140ee1623df89ea9323f11cbdeb8
Max Weng <max.m.weng@gmail.com>
2023-05-08 15:42:57 +0800

    chg: add multiple network support

--- src/bridgeToOp.js
+++ src/bridgeToOp.js
@@ -4,22 +4,44 @@ const {
   DefenderRelayProvider,
 } = require("defender-relay-client/lib/ethers");
 
-const TREASURY_ADDR = "0x8Cf2D155D1b789C72403BAdB33E85664cEF84e6B";
-const CONNECTOR_ADDR = "0xf8e49fAB23884AdFF5499D841010052Ae08Ac8F8";
-const UNION_ADDRESS = "0x23B0483E07196c425d771240E81A9c2f1E113D3A";
+const CONTRACTS = {
+  // mainnet
+  1: {
+    UNION_TOKEN: "0x5Dfe42eEA70a3e6f93EE54eD9C321aF07A85535C",
+    CONNECTOR: "0xF5690129Bf7AD35358Eb2304f4F5B10E0a9B9d65",
+    TREASURY: "0x6DBDe0E7e563E34A53B1130D6B779ec8eD34B4B9",
+  },
+  // goerli
+  5: {
+    UNION_TOKEN: "0x23B0483E07196c425d771240E81A9c2f1E113D3A",
+    CONNECTOR: "0x9b15BFB49961610B65f86a3bCFbBEFEC0AC7bFf1",
+    TREASURY: "0xc124047253c87EF90aF9f4EFC12C281b479c4769",
+  },
+};
 
 exports.handler = async function (payload) {
   const provider = new DefenderRelayProvider(payload);
+  const { chainId } = await provider.getNetwork();
+  console.log({ chainId });
   const signer = new DefenderRelaySigner(payload, provider, {
     speed: "average",
   });
 
-  // Step 1: Drip Union to ArbConnector
-  const treasury = new ethers.Contract(TREASURY_ADDR, TREASURY_ABI, signer);
+  // Step 1: Drip Union to OpConnector
+  const treasury = new ethers.Contract(
+    CONTRACTS[chainId]["TREASURY"],
+    TREASURY_ABI,
+    signer
+  );
+  const CONNECTOR_ADDR = CONTRACTS[chainId]["CONNECTOR"];
   const dripTx = await treasury.drip(CONNECTOR_ADDR);
   await dripTx.wait();
 
-  const union = new ethers.Contract(UNION_ADDRESS, ERC20_ABI, signer);
+  const union = new ethers.Contract(
+    CONTRACTS[chainId]["UNION_TOKEN"],
+    ERC20_ABI,
+    signer
+  );
   const balanceOfUnion = await union.balanceOf(CONNECTOR_ADDR);
   console.log(
     `Connector UNION balance: ${ethers.utils.formatEther(balanceOfUnion)}`
